d2c9f5664d421cd558e07e2ae6ccfd40234476b3
vipin <www.vipinkumar27@gmail.com>
2021-07-10 03:22:52 +0530

    graph

---
+++ LevelUp/4_Graph/ConstructGraph.java
@@ -0,0 +1,50 @@
+
+import java.util.*;
+
+public class ConstructGraph {
+    public static class Edge {
+        int v = 0, w = 0;
+
+        Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    public static void addEdge(ArrayList<Edge>[] graph, int u, int v, int w) {
+        graph[u].add(new Edge(v, w));
+        graph[v].add(new Edge(u, w));
+    }
+    //O(E)
+    public static void display(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            System.out.print(i + " -> ");
+            for (Edge e : graph[i])
+                System.out.print("(" + e.v + ", " + e.w + ") ");
+            System.out.println();
+        }
+    }
+
+    public static void constructGraph() {
+        int N = 7;
+        ArrayList<Edge>[] graph = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        addEdge(graph, 0, 1, 10);
+        addEdge(graph, 1, 2, 10);
+        addEdge(graph, 2, 3, 40);
+        addEdge(graph, 3, 0, 10);
+        addEdge(graph, 3, 4, 2);
+        addEdge(graph, 4, 5, 2);
+        addEdge(graph, 5, 6, 3);
+        addEdge(graph, 6, 4, 8);
+
+        display(graph);
+
+    }
+
+    public static void main(String[] args) {
+        constructGraph();
+    }
+}
